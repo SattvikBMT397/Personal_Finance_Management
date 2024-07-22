9b699258ac567f6e0a6df449807cc97ed4198f98
SattvikBmt397 <sattvik@binmile.com>
2024-07-22 18:07:18 +0530

    establishment

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,10 +5,9 @@ import ContactMailIcon from '@mui/icons-material/ContactMail';
 import GroupIcon from '@mui/icons-material/Group';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
-import DashboardIcon from '@mui/icons-material/Dashboard'
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
-import Images from './Logo/Budget Buddy.png';
-import MobileImage from "./Logo/mobile.png";
+import Images from './Logo/Budget Buddy.png'
 import './Home.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
@@ -20,25 +19,14 @@ const Home = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const currentUser = useSelector((state: RootState) => state.auth.currentUser);
-  const handleLogin = () => {
-    navigate('/login');
-  };
 
-  const handleRegister = () => {
-
-    navigate('/signup');
-  };
-
-  const handleLogout = () => {
-    dispatch(logout());
-  }
-
-  const handleDashboard = () => {
-    navigate('/dashboard');
-  }
+  const handleLogin = () => navigate('/login');
+  const handleRegister = () => navigate('/signup');
+  const handleLogout = () => dispatch(logout());
+  const handleDashboard = () => navigate('/dashboard');
 
   return (
-    <div className='main' style={{ background: 'linear-gradient(180deg, rgba(40, 161, 151, 0.03) 0%, rgba(15, 59, 55, 0.03) 100%)' }}>
+    <div className='main'>
       <div className="containers">
         <img src={Images} alt='Myimages' className="logo" style={{ display: isMobile ? 'none' : 'block' }} />
         <div className='main_middle'>
@@ -57,44 +45,32 @@ const Home = () => {
               {isMobile || isIpad ? <GroupIcon /> : 'Our Teams'}
             </Link>
           </h2>
-          
         </div>
 
-        {
-          currentUser ? (
-            <div className='header_end'>
-              <button onClick={handleLogout} className='btn2'> {isMobile || isIpad ? <LogoutIcon /> : 'Logout'}</button>
-              <button onClick={handleDashboard} className='btn2'>{isMobile || isIpad ? <DashboardIcon/> : 'DashBoard'} </button>
-            </div>
-            
-          ): (
-              <div className='header_end'>
-          <button onClick={handleLogin} className='btn2'>
-            {isMobile || isIpad ? <LoginIcon /> : 'Login'}
-          </button>
-          <button onClick={handleRegister} className='btn2'>
-            {isMobile || isIpad ? <AppRegistrationIcon /> : 'Register'}
-          </button>
-        </div>
-          )
-        }
+        {currentUser ? (
+          <div className='header_end'>
+            <button onClick={handleLogout} className='btn2'>{isMobile || isIpad ? <LogoutIcon /> : 'Logout'}</button>
+            <button onClick={handleDashboard} className='btn2'>{isMobile || isIpad ? <DashboardIcon /> : 'Dashboard'}</button>
+          </div>
+        ) : (
+          <div className='header_end'>
+            <button onClick={handleLogin} className='btn2'>{isMobile || isIpad ? <LoginIcon /> : 'Login'}</button>
+            <button onClick={handleRegister} className='btn2'>{isMobile || isIpad ? <AppRegistrationIcon /> : 'Register'}</button>
+          </div>
+        )}
       </div>
       <div className='secondMiddle'>
         <div className='contents'>
           <div className='inner'>
-            <span className='innerText'>Expense </span><span>Tracker..</span>
-            <p> Your Personal Assistant with<div className='innerText'> Tips, Reminders, and Advice</div></p>
+            <span className='innerText'>Expense</span><span> Tracker..</span>
+            <p>Your Personal Assistant with<div className='innerText'> Tips, Reminders, and Advice</div></p>
           </div>
           <div className='contents2'>
-            <p className='content2text'> Welcome Guy!</p>
+            <p className='content2text'>Welcome Guys!</p>
             <p className='description'>Budget Buddy is here to support you on your journey to financial freedom, offering tips, reminders, and personalized advice along the way.</p>
           </div>
         </div>
-        {!isMobile && !isIpad && (
-          <div className='photos1'>
-            <img className='MobileImage' src={MobileImage} width={400} height={460} alt='Mobile'></img>
-          </div>
-        )}
+        
       </div>
     </div>
   );
--- src/pages/Dashboard/ExpensesChart.tsx
+++ src/pages/Dashboard/ExpensesChart.tsx
@@ -8,22 +8,19 @@ const ExpensesChart: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const chartRef = useRef<Chart | null>(null);
 
-   
     const transactions = useSelector((state: RootState) => state.auth.currentUser?.transaction || []);
 
-   
     const expenseTransactions = transactions.filter((transaction: Expense) => transaction.type === 'expense');
 
     const totalExpenses = expenseTransactions.reduce((acc, expense) => acc + (expense.cost || 0), 0);
 
     useEffect(() => {
         if (canvasRef.current) {
-        
             if (chartRef.current) {
                 chartRef.current.destroy();
             }
 
-            const aggregatedExpenses: Expense[] = expenseTransactions.reduce((acc: Expense[], expense) => {
+            const aggregatedExpenses = expenseTransactions.reduce((acc: Expense[], expense) => {
                 const found = acc.find(item => item.category === expense.category);
                 if (found) {
                     found.cost = (found.cost || 0) + (expense.cost || 0);
@@ -37,15 +34,14 @@ const ExpensesChart: React.FC = () => {
             const amounts = aggregatedExpenses.map(expense => expense.cost);
 
             chartRef.current = new Chart(canvasRef.current, {
-                type: 'line',
+                type: 'bar',
                 data: {
                     labels: categories,
                     datasets: [{
                         label: 'Expenses',
                         data: amounts,
                         borderColor: '#f44336',
-                        backgroundColor: 'rgba(244, 67, 54, 0.2)',
-                        fill: true,
+                        backgroundColor: 'rgba(244, 67, 54, 0.2)'
                     }],
                 },
                 options: {
@@ -71,9 +67,9 @@ const ExpensesChart: React.FC = () => {
 
     return (
         <div style={{ height: '315px', width: '100%' }}>
-            <canvas style={{ maxHeight: '100%', maxWidth: '100%',  marginTop:'100px'}} ref={canvasRef} />
+            <canvas style={{ maxHeight: '100%', maxWidth: '100%', marginTop: '100px' }} ref={canvasRef} />
             <div style={{ textAlign: 'center', marginTop: '50px' }}>
-                <div style={{ display: 'flex', alignItems: 'center',}}>
+                <div style={{ display: 'flex', alignItems: 'center' }}>
                     <h3 style={{ marginTop: '9px', marginLeft: '40px' }}>🧾Total Expenses: ₹{totalExpenses.toFixed(0)}</h3>
                 </div>
             </div>
